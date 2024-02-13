bcc7d089b83742be36281b55b5573796a57625cf
LeonidasJD <milandjordjevic633@gmail.com>
2024-02-13 19:43:19 +0100

    kreirane zasebne styled komponente

---
+++ src/css/Input.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  width: 100%;
+  padding: 0.75rem 1rem;
+  line-height: 1.5;
+  background-color: #d1d5db;
+  color: #374151;
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+
+  .invalid {
+    color: #ef4444;
+    border-color: #f73f3f;
+    background-color: #fed2d2;
+  }
+`;
+
+export default Input;
